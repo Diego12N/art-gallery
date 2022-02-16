9e398cc54ef1d56cd99c75c8c600b4717c7c8fd0
Diego Naranjo <diego.emma.n@outlook.com>
2022-02-15 23:41:05 -0300

    Clase 7 ItemDetail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
